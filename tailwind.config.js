ba697a7f9524447019d88e27501d47e018f0df62
denkochev <denkochev78@gmail.com>
2023-07-14 22:52:57 +0200

    Recomposite some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,33 @@ module.exports = {
     extend: {
       colors: {
         darkBg: "hsl(222, 26%, 31%)",
-        lightBg: "hsl(0, 0%, 90%)",
         purpleBg: "hsl(268, 75%, 9%)",
+        /*
+        // BACKGROUNDS
+        */
+        // main background
+        lightBg: "hsl(0, 0%, 90%)",
+        // keypad background
+        lightKeyPadBG: "hsl(0, 5%, 81%)",
+        // screen background
+        lightGrayDspl: "hsl(0, 0%, 93%)",
+        /*
+        // KEYS COLORS
+        */
+        // key background
+        lightKeyBg: "hsl(185, 42%, 37%)",
+        lightKeyShadow: "hsl(185, 58%, 25%)",
+        lighKeyHover: "hsl(184, 40%, 56%)",
+        lightOrange: "hsl(25, 98%, 40%)",
+        lightDarkOrange: "hsl(25, 99%, 27%)",
+        lightOrangeHover: "hsl(25, 100%, 60%)",
+        controlYellow: "hsl(45, 7%, 89%)",
+        controlOrange: "hsl(35, 11%, 61%)",
+        /*
+        // TEXT COLORS
+        */
+        darkYellow: "hsl(60, 10%, 19%)",
+        prjWhite: "hsl(0, 0%, 100%)",
       },
     },
   },
