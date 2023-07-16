e2d397a8f6c94ac0257844661a203982632822a6
denkochev <denkochev78@gmail.com>
2023-07-16 11:20:24 +0200

    add dark theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
         lightKeyPadBG: "hsl(0, 5%, 81%)",
         // screen background
         lightGrayDspl: "hsl(0, 0%, 93%)",
+        desaturedBlue: "hsl(224, 36%, 15%)",
         /*
         // KEYS COLORS
         */
@@ -36,11 +37,20 @@ module.exports = {
         controlOrange: "hsl(35, 11%, 61%)",
         hoverRed: "hsl(6, 92%, 66%)",
         desterBlue: "hsl(223, 31%, 20%)",
+        graylishOrange: "hsl(30, 25%, 89%)",
+        graylishOrangeHover: "hsl(28, 16%, 65%)",
+        darkDarkBlue: "hsl(225, 21%, 49%)",
+        darkShadowBlue: "hsl(224, 28%, 35%)",
+        darkHoverBlue: "hsl(223, 52%, 75%)",
+        darkRed: "hsl(6, 63%, 50%)",
+        darkShadowRed: "hsl(6, 70%, 34%)",
+        darkHoverRed: "hsl(6, 92%, 66%)",
         /*
         // TEXT COLORS
         */
         darkYellow: "hsl(60, 10%, 19%)",
         prjWhite: "hsl(0, 0%, 100%)",
+        darkGraylish: "hsl(221, 14%, 31%)",
       },
     },
   },
