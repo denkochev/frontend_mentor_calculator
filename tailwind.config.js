9bcb64a9ba584a5d601997fe5ce58bda56767eb1
denkochev <denkochev78@gmail.com>
2023-07-23 21:44:26 +0200

    Add colors for purple theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import plugin from 'tailwindcss/plugin';
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -20,6 +20,7 @@ module.exports = {
         lightBg: "hsl(0, 0%, 90%)",
         // keypad background
         lightKeyPadBG: "hsl(0, 5%, 81%)",
+        purpleDarkViolet: "hsl(268, 71%, 12%)",
         // screen background
         lightGrayDspl: "hsl(0, 0%, 93%)",
         desaturedBlue: "hsl(224, 36%, 15%)",
@@ -45,15 +46,32 @@ module.exports = {
         darkRed: "hsl(6, 63%, 50%)",
         darkShadowRed: "hsl(6, 70%, 34%)",
         darkHoverRed: "hsl(6, 92%, 66%)",
+        pureCyan: "hsl(176, 100%, 44%)",
+        pureCyanShadow: "hsl(177, 92%, 70%)",
+        pureCyanHover: "hsl(176, 100%, 79%)",
+        darkPurpleViolet: "hsl(268, 47%, 21%)",
+        darkMagenta: "hsl(290, 70%, 36%)",
+        darkVioletPurple: "hsl(281, 89%, 26%)",
+        shadowPurple: "hsl(285, 91%, 52%)",
+        purpleHoverViolet: "hsl(267, 53%, 43%)",
+        purpleHoverControl: "hsl(280, 56%, 44%)",
         /*
         // TEXT COLORS
         */
         darkYellow: "hsl(60, 10%, 19%)",
         prjWhite: "hsl(0, 0%, 100%)",
         darkGraylish: "hsl(221, 14%, 31%)",
+        lightYellow: "hsl(52, 100%, 62%)",
+        purpleDarkBlue: "hsl(198, 20%, 13%)",
       },
     },
   },
   plugins: [
+    plugin(function({addVariant}) {
+      // here is your CSS selector - could be anything
+      // in this case it is `.theme` element
+      // with `.theme--red` class (both present)
+      addVariant('purple', '.purple.purple &')
+    }),
   ],
 };
